9e758803882084ba0a46c86b0e88b2207b8b6977
Mikhail Salnikov <salnikovmishanew@gmail.com>
2026-01-09 19:24:45 +0300

    Added few colors and images

--- app/src/main/java/com/example/learning_application_kotlin/ui/theme/Color.kt
+++ app/src/main/java/com/example/learning_application_kotlin/ui/theme/Color.kt
@@ -11,4 +11,11 @@ object DefaultColors {
     val Purple40 = Color(0xFF6650a4)
     val PurpleGrey40 = Color(0xFF625b71)
     val Pink40 = Color(0xFF7D5260)
+
+    val DarkGray = Color(0xFFA9A9A9)
+
+    val MuchDarker = Color(0xFF757575)
+
+    val LightDark = Color(0xFFADADAD)
+
 }
