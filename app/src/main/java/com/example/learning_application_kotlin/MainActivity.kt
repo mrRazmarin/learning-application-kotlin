cffe6ab6f63fc2490a04aea4ea7255f1601c6ba4
Mikhail Salnikov <salnikovmishanew@gmail.com>
2026-01-09 19:26:50 +0300

    MainScreen and mainactivity

--- app/src/main/java/com/example/learning_application_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/learning_application_kotlin/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.learning_application_kotlin.layout_containers.layouts.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            MainScreen()
         }
     }
 }
